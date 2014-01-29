e7e85875612fc1ad085b2a4eb124e4687b2c414b
Owen Solberg <owen@myway.com>
2014-01-29 13:52:21 -0800

    added footnote function

--- OwensFunctions.R
+++ OwensFunctions.R
@@ -1,4 +1,15 @@
-options(width=120)
+
+makeFootnote <- function(footnoteText=format(Sys.time(), "%d %b %Y"), size= .7, color= grey(.5)) {
+  require(grid)
+  pushViewport(viewport())
+  grid.text(label= footnoteText ,
+            x = unit(1,"npc") - unit(2, "mm"),
+            y= unit(2, "mm"),
+            just=c("right", "bottom"),
+            gp=gpar(cex= size, col=color))
+  popViewport()
+}
+
 
 iqr <- function(x, ...) {
   q2 <- quantile(x, .25, na.rm=TRUE)

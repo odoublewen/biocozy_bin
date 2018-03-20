088219c84e4f776c46a9962b9b3d031f06f08c70
Owen Solberg <odoublewen@users.noreply.github.com>
2018-03-20 16:17:55 -0700

    Update git-change-date.sh

--- git-change-date.sh
+++ git-change-date.sh
@@ -4,11 +4,15 @@ REF_WITH_DATE=$2
 COMMIT_HASH=`git show -s --format="%H" $REF_TO_UPDATE`
 NEW_DATE=`git show -s --format=%ci $REF_WITH_DATE`
 
-echo "Following command will update $COMMIT_HASH to $NEW_DATE"
 
 read -d '' COMMAND <<- EOF
 git filter-branch -f --env-filter 'if [ \$GIT_COMMIT = $COMMIT_HASH ]; then export GIT_AUTHOR_DATE="$NEW_DATE"; export GIT_COMMITTER_DATE="$NEW_DATE"; fi'
 EOF
 
+tfile=$(mktemp /tmp/foo.XXXXXXXXX)
+
+echo "Following command in file ${tfile} will update $COMMIT_HASH to $NEW_DATE"
 echo $COMMAND
-$COMMAND
+echo $COMMAND > ${tfile}
+
+bash ${tfile}
